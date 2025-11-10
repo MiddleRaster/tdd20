2af40f05dfc1342f7263e0b2cda112820b8a128e
MiddleRaster <235772580+MiddleRaster@users.noreply.github.com>
2025-11-10 10:11:23 -0500

    tighted up code; everything is a one-liner now.

--- main/main.cpp
+++ main/main.cpp
@@ -3,9 +3,7 @@ import tdd20;
 
 int main(int argc, char* argv[])
 {
-	if (argc > 1)
-	if (argv[1] == std::string("-dump") ||
-		argv[1] == std::string("/dump") )
+	if ((argc > 1) && (argv[1] == std::string("-dump") || argv[1] == std::string("/dump")))
 	{
 		struct Dumper { bool WantTest  (const std::string& name) const { std::cout << name << "\n"; return false; } };
 		struct Noop   { void operator<<(const std::string&) {} };
@@ -15,20 +13,10 @@ int main(int argc, char* argv[])
 
 	class CmdLineMatcher
 	{
-		auto MakeVector(int argc, char* argv[])
-		{
-			std::vector<std::string> v;
-			for(int i=1; i<argc; ++i)
-				v.push_back(argv[i]);
-			return v;
-		}
 		const std::vector<std::string> args;
 	public:
-		CmdLineMatcher(int argc, char* argv[]) : args(MakeVector(argc, argv)) {}
-		bool WantTest(const std::string& name) const
-		{
-			return args.size() == 0 ? true : args.cend() != std::find(args.cbegin(), args.cend(), name);
-		}
+		CmdLineMatcher(int argc, char* argv[]) : args(argv+1, argv+argc) {}
+		bool WantTest(const std::string& name) const { return args.size() == 0 ? true : args.cend() != std::find(args.cbegin(), args.cend(), name); }
 	};
 	auto [passed, failed] = TDD20::TestRegistrar::RunTests(CmdLineMatcher{argc, argv}, std::cout);
     return 0;
