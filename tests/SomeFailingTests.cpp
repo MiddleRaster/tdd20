bdd8e1d7c7d5b2d60f5dcaf4e25670a8de6bdb87
MiddleRaster <235772580+MiddleRaster@users.noreply.github.com>
2025-11-10 17:20:35 -0500

    threw logic exception, not invalid arg exception

--- tests/SomeFailingTests.cpp
+++ tests/SomeFailingTests.cpp
@@ -5,7 +5,7 @@ Test someFailingTests[] =
 {
 	{"generic pointer test",                                                                  []{ struct X {} a,b; Assert::AreEqual(&a, &b); } }, // will compare hex addresses
 	{"no file/line info when catching an exception that doesn't inherit from std::exception", []{ throw 7; } },
-	{"no file/line info when catching an exception that does    inherit from std::exception", []{ throw std::invalid_argument("invalid argument exception"); } },
+	{"no file/line info when catching an exception that does    inherit from std::exception", []{ throw std::logic_error ("logic error exception"); } },
 	{"just failing right off",                                                                []{ Assert::Fail       ("Yep, failed"); } },
 	{"an Assert::AreNotEqual test",                                                           []{ Assert::AreNotEqual("hello", "hello"); }},
 	{"an IsWithin test using 15 decimal places",                                              []{ Assert::AreWithin  (std::numbers::pi, 3.14, 0.0001, "not close enough"); } },
